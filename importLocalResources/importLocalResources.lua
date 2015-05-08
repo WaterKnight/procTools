353d23d9e763270aade60d7309eafcb384680445
WaterKnight <awaterknight@googlemail.com>
2015-05-08 19:24:12 +0200

    new version

---
+++ importLocalResources/importLocalResources.lua
@@ -0,0 +1,101 @@
+local params = {...}
+
+local mapPath = params[1]
+local lookupPath = params[2]
+
+assert(mapPath, 'no mapPath')
+assert(lookupPath, 'no lookupPath')
+
+require 'waterlua'
+
+lookupPath = io.toFolderPath(lookupPath)
+
+osLib.clearScreen()
+
+local modelPaths = {}
+
+require 'wc3objMerge'
+
+local objCon = objLib.createInstance()
+
+objCon:readFromMap(mapPath, false)
+
+for objId, objData in pairs(objCon.objs) do
+	for field, fieldData in pairs(objData.fields) do
+		for level, levelData in pairs(fieldData) do
+			local val = levelData.val
+
+			if (type(val) == 'string') then
+				modelPaths[val] = val
+			end
+		end
+	end
+end
+
+require 'portLib'
+
+local extPort = createMpqPort()
+
+extPort:addExtract('war3map.wts', 'war3map.wts')
+extPort:addExtract('war3map.j', 'war3map.j')
+
+extPort:commit(mapPath)
+
+require 'wc3jass'
+
+local j = createJass()
+
+j:readFromPath('war3map.j')
+
+local function searchJassLine(line)
+	local lits = line:gmatch([[%"(.-)%"]])
+
+	for lit in lits do
+		lit = lit:gsub([[\\]], [[\]])
+
+		modelPaths[lit] = lit
+	end
+end
+
+for _, glob in pairs(j.globals) do
+	if (glob.val ~= nil) then
+		searchJassLine(glob.val)
+	end
+end
+
+for _, func in pairs(j.funcs) do
+	for _, line in pairs(func.lines) do
+		searchJassLine(line)
+	end
+end
+
+require 'wtsParser'
+
+local parseTable = wtsParser.parse('war3map.wts')
+
+local t = copyTable(modelPaths)
+
+for path in pairs(t) do
+	modelPaths[path] = nil
+
+	path = wtsParser.translateString(parseTable, path)
+
+	if path:match([[^Local\]]) then
+		modelPaths[path] = path
+	end
+end
+
+local impPort = createMpqPort()
+
+for _, path in pairs(modelPaths) do
+	local diskPath = io.toAbsPath(path, lookupPath)
+
+	if io.pathExists(diskPath) then
+		print('import', diskPath, 'to', path)
+		impPort:addImport(diskPath, path)
+	else
+		print(path, 'not found')
+	end
+end
+
+impPort:commit(mapPath)
